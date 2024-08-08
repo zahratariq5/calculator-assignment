4cdc3bf82526615a26494bf552a65cf86f9d0ca3
zahratariq5 <zahratariq754@gmail.com>
2024-08-08 21:41:17 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+#addition
+number1 = 2
+number2 = 5
+add_result =number1+number2
+#subtract
+number1 = 2
+number2 = 5
+subtract_result =number1-number2
+#multiplication
+number1 = 2
+number2 = 5
+multiply_result =number1*number2
+#division
+number1= 2
+number2 = 5
+divide_result =number1/number2
+#print variables
+print(f"this output is addition value : {add_result}")
+print(f"this output is subtract value : {subtract_result}")
+print(f"this output is multiply value : {multiply_result}")
+print(f"this output is division value : {divide_result}")
